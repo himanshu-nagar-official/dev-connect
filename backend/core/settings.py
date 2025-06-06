eaebe732c66d78b3b5ca290fdef799c43e3be17d
Himanshu Nagar <himanshunagarofficial@gmail.com>
2025-06-06 21:43:34 +0530

    Configure PostgreSQL with Django, run initial migrations, create superuser

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devconnect_db',
+        'USER': 'devuser',
+        'PASSWORD': 'devpass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
