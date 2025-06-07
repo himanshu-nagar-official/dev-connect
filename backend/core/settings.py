6785843fc7626326b6fe61b4f230afe87e628a61
Himanshu Nagar <himanshunagarofficial@gmail.com>
2025-06-07 10:53:43 +0530

    Register users app and install Django REST Framework

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'rest_framework', #We'll need this soon
 ]
 
 MIDDLEWARE = [
