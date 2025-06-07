14875295082cae6ef628a5da2ac786233f7be191
Himanshu Nagar <himanshunagarofficial@gmail.com>
2025-06-07 11:13:52 +0530

    Add UserProfile model and expose RESTful API

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
